b95cd9e40725945425d6dc43ce0e220b51d81d07
Your Name <you@example.com>
2020-12-02 03:10:54 +0200

    init commit

---
+++ src/main/java/com/example/RestAPIServices/RestApiServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestAPIServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestAPIServices/RestApiServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestAPIServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
